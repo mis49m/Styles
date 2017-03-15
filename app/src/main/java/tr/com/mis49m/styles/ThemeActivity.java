151aab7dd91b52be7fc416489ad906811a3faa9e
Ümit TOPAÇAN <UMITTOPACAN@kkb.com.tr>
2017-03-15 19:03:08 +0200

    Initial commit

---
+++ app/src/main/java/tr/com/mis49m/styles/ThemeActivity.java
@@ -0,0 +1,13 @@
+package tr.com.mis49m.styles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ThemeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme);
+    }
+}
